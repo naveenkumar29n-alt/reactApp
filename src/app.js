48ab08763b6f314eb16000b186e44025ac1e92b2
naveenkumar2929n <naveenkumar2929n@gmail.com>
2025-10-15 12:23:38 +0530

    react element & functional component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading=React.createElement("h1",{id:"heading"},"hello world react🚀");
+const heading=<h1 id="heading">hello world🚀</h1>;
+const Footer=()=>{
+  return(
+    <div className="footer">
+      {heading}
+    </div>
+  )
+}
+const Body=()=>{
+  return(
+    <div className="body">
+      {<h1>{1000+1}</h1>}
+      {Footer()}
+    </div>
+  )
+}
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<Body/>)
+
+
