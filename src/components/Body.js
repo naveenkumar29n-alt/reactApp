91783334f596606923b9935f3e19caa2cb4739af
naveenkumar2929n <naveenkumar2929n@gmail.com>
2025-10-25 01:24:39 +0530

    api call using useEffect hook and shiimer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body=()=>{
-  const[listOfRestaurants,setListOfRestaurants]=useState(resList)
-  return(
+  const[listOfRestaurants,setListOfRestaurants]=useState([]);
+  useEffect(()=>{
+    fetchData()
+  },[])
+  const fetchData=async()=>{
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.0102&lng=76.9701&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json=await data.json();
+    setListOfRestaurants(json?.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+  }
+  return (listOfRestaurants.length===0)?<Shimmer/>:(
     <div className="body">
       <div className="filter">
         <button className="filter-btn" onClick={()=>{
