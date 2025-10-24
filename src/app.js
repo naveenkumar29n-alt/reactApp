7df249f68aa5d2f382c96ae7dc482dc5f02f9fc2
naveenkumar2929n <naveenkumar2929n@gmail.com>
2025-10-25 00:46:12 +0530

    react element &react functional component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading=React.createElement("h1",{id:"heading"},"hello React🚀");
+const heading=<h1 id="heading">hello world🚀</h1>
+const Footer=()=>{
+  return(
+    <div className="footer">
+      <h1>Footer component</h1>
+    </div>
+  )
+}
+const Body=()=>{
+  return(
+    <div className="body">
+      {heading}
+      <h1>body component</h1>
+      {Footer()}
+    </div>
+  )
+}
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<Body/>)
+console.log(heading);
+console.log(<Body/>);
